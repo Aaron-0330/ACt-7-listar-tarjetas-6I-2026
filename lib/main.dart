fbe3c1416c765cf8304d5012cf97bb0387c84ddc
a.23308051280569 <a.23308051280569@cbtis128.edu.mx>
2026-02-11 20:31:21 +0000

    Corrección de errores y mejoras de rendimiento

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,104 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const AppHAGALO());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppHAGALO extends StatelessWidget {
+  const AppHAGALO({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Lasherramientas(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Lasherramientas extends StatelessWidget {
+  const Lasherramientas({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF1A1A1A),
+        title: const Text(
+          "HAGALO",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.build, color: Colors.orangeAccent),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.chair, color: Colors.cyanAccent),
+            tooltip: 'Muebles',
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.deck, color: Colors.lightGreenAccent),
+            tooltip: 'Decoración Exterior',
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.kitchen, color: Colors.pinkAccent),
+            tooltip: 'Electrodomésticos',
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          _crearTarjeta("Aaron Dominguez", " Vendedor de Sofás y mesas de centro",
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjX4yY5hWW90ynw6eanlwuSyR1dwddVMGxWQ&s"),
+          _crearTarjeta("Decoración Exterior", "Macetas y muebles de jardín",
+              "https://cdn-icons-png.flaticon.com/512/358/358824.png"),
+          _crearTarjeta("Electrodomésticos", "Lo último en tecnología",
+              "https://raw.githubusercontent.com/Aaron-0330/Imagenes-para-flutter-6to-I-fecha-11-de-febrero-2026/refs/heads/main/navaherramienta3png.png"),
+          _crearTarjeta("Iluminación", "Lámparas y focos LED",
+              "https://picsum.photos/id/42/200"),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearTarjeta(String titulo, String subtitulo, String urlImagen) {
+    return Card(
+      color: const Color(0xFF1E1E1E),
+      elevation: 10,
+      margin: const EdgeInsets.only(bottom: 20),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: ListTile(
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        leading: CircleAvatar(
+          radius: 30,
+          backgroundColor: Colors.grey[900],
+          backgroundImage: NetworkImage(urlImagen),
+        ),
+        title: Text(
+          titulo,
+          style: const TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(
+          subtitulo,
+          style: const TextStyle(color: Colors.white70),
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.favorite, color: Color(0xFFB71C1C)),
+          onPressed: () {
+            debugPrint("Like en: $titulo");
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
